259c831e9d09a12b80a3d00086515881553c7d16
sunnyworm <sunnyworm@gmail.com>
2016-01-24 19:44:28 +0800

    change default canvas size to 176:120
    Former-commit-id: 1af6c87396615deeeac5625c043310d774217323
    Former-commit-id: d467bfe12f6f37dc3b98de285027fcf502fdeea8

--- Player/Player.js
+++ Player/Player.js
@@ -204,8 +204,8 @@ p.decode(<binary>);
     if (!this._config.size){
       this._config.size = {};
     };
-    this._config.size.width = this._config.size.width || 320;
-    this._config.size.height = this._config.size.height || 240;
+    this._config.size.width = this._config.size.width || 176;
+    this._config.size.height = this._config.size.height || 120;
     
     this.canvas = document.createElement('canvas');
     this.canvas.width = this._config.size.width;
